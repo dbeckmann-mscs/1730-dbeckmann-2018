fda760867f7a0375718a119141f8617a5e0331c8
dbeckmann-mscs <42790179+dbeckmann-mscs@users.noreply.github.com>
2018-09-05 21:14:04 -0500

    Created ex 1B

--- dbeckmann 1B1/Program.cs
+++ dbeckmann 1B1/Program.cs
@@ -16,7 +16,7 @@ namespace dbeckmann_1B1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmInvoiceTotal());
         }
     }
 }
---
+++ dbeckmann 1B1/frmInvoiceTotal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dbeckmann_1B1
+{
+    public partial class frmInvoiceTotal : Form
+    {
+        public frmInvoiceTotal()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            ////txtTotal.Text = "10";
+            ////txtTotal.ReadOnly = false;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
